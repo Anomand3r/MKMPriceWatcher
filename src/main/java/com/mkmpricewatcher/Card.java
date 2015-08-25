bb5c54cb36e7a518726ec0b7f5297f96a79aeb1d
Alexandru Cojocaru <cjcr_alexandru@yahoo.com>
2015-08-25 17:39:14 +0300

    Imported the initial cards. Implemented the obtaining of the cards from the database.

--- src/main/java/com/mkmpricewatcher/Card.java
+++ src/main/java/com/mkmpricewatcher/Card.java
@@ -1,7 +1,20 @@
 package com.mkmpricewatcher;
 
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
 public class Card {
     private String name;
+    private String set;
+    private int popularity = -1;
+    private double buyPrice;
+    private LocalDateTime buyDate;
+    private double buyThreshold = -1.0;
+    private double currentPrice = -1.0;
+    private double sellPrice = -1.0;
+    private LocalDateTime sellDate;
+    private double sellThreshold = -1.0;
+    private int quantity = -1;
 
     public void setName(String name) {
         this.name = name;
@@ -9,5 +22,102 @@ public class Card {
 
     public String getName() {
         return name;
+    }
+
+    public void setSet(String set) {
+        this.set = set;
+    }
+
+    public String getSet() {
+        return set;
+    }
+
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public LocalDateTime getBuyDate() {
+        return buyDate;
+    }
+
+    public void setBuyDate(LocalDateTime buyDate) {
+        this.buyDate = buyDate;
+    }
+
+    public double getBuyThreshold() {
+        return buyThreshold;
+    }
+
+    public void setBuyThreshold(double buyThreshold) {
+        this.buyThreshold = buyThreshold;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public LocalDateTime getSellDate() {
+        return sellDate;
+    }
+
+    public void setSellDate(LocalDateTime sellDate) {
+        this.sellDate = sellDate;
+    }
+
+    public double getSellThreshold() {
+        return sellThreshold;
+    }
+
+    public void setSellThreshold(double sellThreshold) {
+        this.sellThreshold = sellThreshold;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "name='" + name + '\'' +
+                ", set='" + set + '\'' +
+                ", popularity=" + popularity +
+                ", buyPrice=" + buyPrice +
+                ", buyDate=" + buyDate +
+                ", buyThreshold=" + buyThreshold +
+                ", currentPrice=" + currentPrice +
+                ", sellPrice=" + sellPrice +
+                ", sellDate=" + sellDate +
+                ", sellThreshold=" + sellThreshold +
+                ", quantity=" + quantity +
+                '}';
     }
 }
--- src/main/java/com/mkmpricewatcher/PriceWatcher.java
+++ src/main/java/com/mkmpricewatcher/PriceWatcher.java
@@ -7,14 +7,16 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import javax.net.ssl.*;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,30 +27,110 @@ public class PriceWatcher {
 
     public static void main(String[] args) {
         installAllTrustingManager();
+        updateCards(getCardsFromDB());
+//        importInitialCards();
+
+    }
+
+    private static void updateCards(List<Card> cards) {
+        System.out.println(cards);
+    }
+
+    private static List<Card> getCardsFromDB() {
+        List<Card> cards = new ArrayList<>();
         try {
             Connection connection = DriverManager.getConnection(DATABASE_CONNECTION_STRING);
-            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM CARD_SET");
-            resultSet.next();
-            String set = resultSet.getString("NAME");
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM CARD");
+            while (resultSet.next()) {
+                cards.add(createCardFromResultSet(resultSet));
+            }
             connection.close();
+        } catch (SQLException e) {
+            logger.error("An error has occurred while trying to get the cards from the database", e);
+        }
+
+        return cards;
+    }
+
+    private static Card createCardFromResultSet(ResultSet resultSet) throws SQLException {
+        Card card = new Card();
+        card.setName(resultSet.getString("NAME"));
+        card.setSet(resultSet.getString("CARD_SET"));
+        card.setCurrentPrice(resultSet.getDouble("CURRENT_PRICE"));
+        card.setBuyPrice(resultSet.getDouble("BUY_PRICE"));
+        String buyDateString = resultSet.getString("BUY_DATE");
+        if (buyDateString != null) {
+            card.setBuyDate(DateTimeFormatter.ISO_DATE_TIME.parse(buyDateString, LocalDateTime::from));
+        }
+        card.setBuyThreshold(resultSet.getDouble("BUY_THRESHOLD"));
+        card.setSellPrice(resultSet.getDouble("SELL_PRICE"));
+        String sellDateString = resultSet.getString("SELL_DATE");
+        if (sellDateString != null) {
+            card.setSellDate(DateTimeFormatter.ISO_DATE_TIME.parse(sellDateString, LocalDateTime::from));
+        }
+        card.setSellThreshold(resultSet.getDouble("SELL_THRESHOLD"));
+        card.setPopularity(resultSet.getInt("POPULARITY"));
+        card.setQuantity(resultSet.getInt("QUANTITY"));
+        return card;
+    }
+
+    private static void importInitialCards() {
+        try {
+            insertCardsInDB(getCardsFromMKM());
+        } catch (SQLException | IOException e) {
+            logger.error("An error has occurred.", e);
+        }
+    }
+
+    private static void insertCardsInDB(List<Card> cards) throws SQLException {
+        Connection c = DriverManager.getConnection(DATABASE_CONNECTION_STRING);
+        PreparedStatement updateCard = c.prepareStatement("INSERT OR REPLACE INTO CARD(NAME, CARD_SET, CURRENT_PRICE, BUY_PRICE, BUY_DATE, BUY_THRESHOLD, SELL_PRICE, SELL_DATE, SELL_THRESHOLD, POPULARITY, QUANTITY, LAST_MODIFIED) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        cards.stream().forEach(card -> {
+            try {
+                updateCard.setString(1, card.getName());
+                updateCard.setString(2, card.getSet());
+                updateCard.setDouble(3, card.getCurrentPrice());
+                updateCard.setDouble(4, card.getBuyPrice());
+                updateCard.setString(5, card.getBuyDate() != null ? card.getBuyDate().format(DateTimeFormatter.ISO_DATE_TIME) : null);
+                updateCard.setDouble(6, card.getBuyThreshold());
+                updateCard.setDouble(7, card.getSellPrice());
+                updateCard.setString(8, card.getSellDate() != null ? card.getSellDate().format(DateTimeFormatter.ISO_DATE_TIME) : null);
+                updateCard.setDouble(9, card.getSellThreshold());
+                updateCard.setInt(10, card.getPopularity());
+                updateCard.setInt(11, card.getQuantity());
+                updateCard.setString(12, LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+                updateCard.executeUpdate();
+            } catch (SQLException e) {
+                logger.error("An error has occurred while trying to insert cards in the database", e);
+            }
+        });
+        c.close();
+        logger.info("Updated " + cards.size() + " cards.");
+    }
+
+    private static List<Card> getCardsFromMKM() throws SQLException, IOException {
+        List<Card> cards = new ArrayList<>();
+        Connection connection = DriverManager.getConnection(DATABASE_CONNECTION_STRING);
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM CARD_SET");
+        while (resultSet.next()) {
+            String set = resultSet.getString("NAME");
             Document document = Jsoup.connect("https://www.magiccardmarket.eu/Products/Singles/" + set + "?idRarity=17&sortBy=popularity&sortDir=desc")
                     .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36")
                     .referrer("http://www.google.com")
                     .get();
             Elements tableRows = document.select("table.MKMTable > tbody > tr");
-            List<Card> cards = new ArrayList<>();
+            int popularity = tableRows.size();
             for (Element tableRow : tableRows) {
                 Elements tableData = tableRow.select("td");
                 Card card = new Card();
                 card.setName(tableData.get(2).select("a").text());
+                card.setSet(set);
+                card.setPopularity(popularity--);
                 cards.add(card);
             }
-            System.out.println(cards.get(0).getName());
-
-        } catch (SQLException | IOException e) {
-            logger.error("An error has occurred.", e);
         }
-
+        connection.close();
+        return cards;
     }
 
     private static void installAllTrustingManager() {
@@ -76,5 +158,6 @@ public class PriceWatcher {
         } catch (NoSuchAlgorithmException | KeyManagementException e) {
             e.printStackTrace();
         }
+        logger.info("Installed all trusting manager");
     }
 }
